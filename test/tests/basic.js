8386120a5ac4e97579069299a15c2b9c18f801fe
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-02-27 16:09:57 +0330

    first build test

--- test/tests/basic.js
+++ test/tests/basic.js
@@ -1,7 +1,6 @@
 const test = require('./../common').test
 const expect = require('chai').expect
 const _send = test.sendMessage
-const TOTAL = require('./../common').TOTAL
 
 let processes
 let identifiers = []
