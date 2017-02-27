c488053d925fe755ac4519b88105cef411704696
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-02-27 16:00:59 +0330

    tests added

---
+++ test/tests/basic.js
@@ -0,0 +1,54 @@
+const test = require('./../common').test
+const expect = require('chai').expect
+const _send = test.sendMessage
+const TOTAL = require('./../common').TOTAL
+
+let processes
+let identifiers = []
+let TESTER
+let lastValue
+before(function (done) {
+  test.setUpTestEnv((p) => {
+    processes = p
+    identifiers = Object.keys(processes)
+    TESTER = test.getTester()
+    done()
+  })
+})
+
+it('initial state', function (done) {
+  _send('pingRate', processes[identifiers[0]], (data) => {
+    expect(data.interval).to.be.within(2000, 5000)
+    done()
+  })
+})
+
+it('interval should increase', function (done) {
+  setTimeout(() => {
+    _send('pingRate', processes[identifiers[0]], (data) => {
+      lastValue = data.interval
+      expect(data.interval).to.be.within(10000, 20000)
+      done()
+    })
+  }, 15000)
+  this.timeout(20000)
+})
+
+it('interval should increase when a node goes down', function (done) {
+  TESTER.call({servicePath: 'node/kill', payload: '1'}, (err, body) => {
+    expect(body).to.equal('Done')
+    setTimeout(() => {
+      _send('pingRate', processes[identifiers[0]], (data) => {
+        expect(data.interval).to.be.below(lastValue)
+        done()
+      })
+    }, 10000)
+  })
+  this.timeout(20000)
+})
+
+after(function () {
+  for (let p in processes) {
+    processes[p].kill()
+  }
+})
---
+++ test/tests/ms/math.ms.js
@@ -0,0 +1,26 @@
+let XYZ = require('xyz-core')
+let fn = require('./../../mock.functions')
+
+var mathMs = new XYZ({
+  selfConf: {
+    name: 'math.ms',
+    host: '127.0.0.1',
+    defaultBootstrap: false
+  },
+  systemConf: {nodes: []}
+})
+
+mathMs.bootstrap(require('./../../../ping.stochastic'), true, mathMs.id().port)
+mathMs.register('/math/decimal/mul', fn.mul)
+mathMs.register('/math/decimal/neg', fn.neg)
+mathMs.register('/math/decimal/sub', fn.sub)
+
+mathMs.register('/math/float/neg', function (payload, XResponse) {
+  XResponse.send('ok whassssaaaap')
+})
+
+setInterval(() => {
+  mathMs.call({servicePath: '/string/up', payload: 'hello'}, (err, body, response) => {
+    console.log(err, body)
+  })
+}, 2000)
